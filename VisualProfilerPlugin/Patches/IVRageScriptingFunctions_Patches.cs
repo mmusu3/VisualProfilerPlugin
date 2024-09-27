4441be12b03155ee2555aeb21fea922532d10b09
Remaarn <mmusu3@live.com.au>
2024-09-27 16:59:08 +1000

    Add patch for IVRageScriptingFunctions.CompileIngameScriptAsync()

---
+++ VisualProfilerPlugin/Patches/IVRageScriptingFunctions_Patches.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using Torch.Managers.PatchManager;
+using VRage.Scripting;
+
+namespace VisualProfiler.Patches;
+
+[PatchShim]
+static class IVRageScriptingFunctions_Patches
+{
+    public static void Patch(PatchContext ctx)
+    {
+        Keys.Init();
+
+        var source = typeof(IVRageScriptingFunctions).GetPublicStaticMethod(nameof(IVRageScriptingFunctions.CompileIngameScriptAsync));
+        var prefix = typeof(IVRageScriptingFunctions_Patches).GetNonPublicStaticMethod(nameof(Prefix_CompileIngameScriptAsync));
+        var suffix = typeof(IVRageScriptingFunctions_Patches).GetNonPublicStaticMethod(nameof(Suffix));
+
+        var pattern = ctx.GetPattern(source);
+        pattern.Prefixes.Add(prefix);
+        pattern.Suffixes.Add(suffix);
+    }
+
+    static class Keys
+    {
+        internal static ProfilerKey CompileIngameScriptAsync;
+
+        internal static void Init()
+        {
+            CompileIngameScriptAsync = ProfilerKeyCache.GetOrAdd("CompileIngameScriptAsync");
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static bool Prefix_CompileIngameScriptAsync(ref ProfilerTimer __local_timer, string program)
+    {
+        __local_timer = Profiler.Start(Keys.CompileIngameScriptAsync, profileMemory: true,
+            new(program.Length, "Program Length: {0}"));
+
+        return true;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static void Suffix(ref ProfilerTimer __local_timer) => __local_timer.Stop();
+}
