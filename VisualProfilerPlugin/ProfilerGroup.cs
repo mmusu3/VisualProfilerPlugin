8a43fed4fbc2df635c7230c6fd82870f8ea7dd55
Remaarn <mmusu3@live.com.au>
2024-12-09 17:45:37 +1000

    Fix invalid last frame start event index being added to recording

--- VisualProfilerPlugin/ProfilerGroup.cs
+++ VisualProfilerPlugin/ProfilerGroup.cs
@@ -542,8 +542,16 @@ public class ProfilerGroup
 
             if (eventCount > 0)
             {
+                var frameStartIndices = frameStartEventIndices.ToArray();
+
+                // The values in frameStartEventIndices point to the next index to be used but if
+                // an event was not yet added there before StopEventRecording was called then the
+                // index will be invalid.
+                if (frameStartIndices.Length > 0 && frameStartIndices[^1] >= eventCount)
+                    frameStartIndices = frameStartIndices.AsSpan(0, frameStartIndices.Length - 1).ToArray();
+
                 recording = new ProfilerEventsRecordingGroup(Name, recordedEvents.Segments, eventCount,
-                    frameStartEventIndices.ToArray(), frameEndEventIndices.ToArray(), outlierFrameIndices.ToArray());
+                    frameStartIndices, frameEndEventIndices.ToArray(), outlierFrameIndices.ToArray());
             }
 
             prevFrameEndEventIndex = -1;
